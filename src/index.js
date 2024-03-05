0fa593e769331111dd131499353384c86ea25a59
mmogesdesigns <130083505+mmogesdesigns@users.noreply.github.com>
2024-03-04 20:57:15 -0800

    injected the forecast html from javascript

--- src/index.js
+++ src/index.js
@@ -59,4 +59,31 @@ function handleSearchSubmit(event) {
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+                    <div class="weather-forecast-date">
+                    ${day}
+                        </div>  
+                    <div class="weather-forecast-icon">
+                        🌤️
+                        </div> 
+                    <div class="weather-forecast-temp">
+                    <span class="weather-forecast-temp-max">18°</span>
+                    <span class="weather-forecast-temp-min">12°</span>    
+                    
+                    </div>
+            </div>`;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 searchCity("Los Angeles");
+displayForecast();
